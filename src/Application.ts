2af7a4d28e7f1dded2b4e50130e8e89244321711
meowlet <ngoclink13@gmail.com>
2024-08-26 21:03:24 +0700

    Feature: Temporarily finished the sign up route

--- src/Application.ts
+++ src/Application.ts
@@ -1,6 +1,7 @@
 import Elysia from "elysia";
 import { UserController } from "./controller/UserController";
 import { AuthController } from "./controller/AuthController";
+import { AuthPlugin } from "./plugin/AuthPlugin";
 
 export const Application = new Elysia()
   .group("/user", (app) => app.use(UserController))
--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -1,6 +1,62 @@
 import Elysia from "elysia";
+import { AuthModel } from "../model/AuthModel";
+import { AuthRepository } from "../repository/AuthRepository";
+import { Database } from "../database/Database";
+import { createErrorResponse, createSuccessResponse } from "../model/Response";
+import {
+  validateEmail,
+  validatePassword,
+  validateUsername,
+} from "../util/InputValidator";
 
-export const AuthController = new Elysia().get(
-  "/signup",
-  async () => "Viewing signup page"
-);
+export const AuthController = new Elysia()
+  .use(AuthModel)
+  .use(Database)
+  .derive(({ database }) => {
+    return {
+      authRepository: new AuthRepository(database),
+    };
+  })
+  .post(
+    "/signup",
+    async ({ body, authRepository, set }) => {
+      const emailResult = validateEmail(body.email);
+      if (!emailResult.isValid) {
+        set.status = 400;
+        return createErrorResponse(
+          "Invalid email",
+          "VALIDATION",
+          emailResult.error || "Invalid email"
+        );
+      }
+
+      const usernameResult = validateUsername(body.username);
+      if (!usernameResult.isValid) {
+        set.status = 400;
+        return createErrorResponse(
+          "Invalid username",
+          "VALIDATION",
+          usernameResult.error || "Invalid usernamme"
+        );
+      }
+
+      const passwordResult = validatePassword(body.password);
+      if (!passwordResult.isValid) {
+        set.status = 400;
+        return createErrorResponse(
+          "Invalid password",
+          "VALIDATION",
+          passwordResult.error || "Invalid password"
+        );
+      }
+
+      await authRepository.signup(body.email, body.username, body.password);
+      return createSuccessResponse<any>(
+        "User signed up successfully",
+        undefined
+      );
+    },
+    {
+      body: "signUp",
+    }
+  );
