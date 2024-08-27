c49cea20bfdc62cfda5871b7d685b00a5df2087c
meowlet <ngoclink13@gmail.com>
2024-08-27 22:47:35 +0700

    Chore: Update AuthModel and AuthPlugin for input validation and error handling

--- src/model/AuthModel.ts
+++ src/model/AuthModel.ts
@@ -6,8 +6,23 @@ export const AuthModel = new Elysia().model({
     password: t.String(),
   }),
   signUp: t.Object({
-    email: t.String(),
-    username: t.String(),
-    password: t.String(),
+    email: t.String({
+      format: "email",
+      error: "Invalid email format",
+    }),
+    username: t.String({
+      minLength: 4,
+      maxLength: 32,
+      pattern: "^[a-zA-Z0-9_]+$",
+      error: "Username must be alphanumeric and contain underscores only",
+    }),
+    password: t.String({
+      minLength: 8,
+      maxLength: 32,
+      pattern:
+        "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+      error:
+        "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+    }),
   }),
 });
--- src/plugin/AuthPlugin.ts
+++ src/plugin/AuthPlugin.ts
@@ -4,6 +4,7 @@ import jwt from "@elysiajs/jwt";
 import { AuthService } from "../service/AuthService";
 import { Database } from "../database/Database";
 import { AuthRepository } from "../repository/AuthRepository";
+import { AuthorizationErrorType } from "../util/Enum";
 
 export const AuthPlugin = new Elysia()
   .use(Database)
@@ -16,12 +17,18 @@ export const AuthPlugin = new Elysia()
   .derive({ as: "global" }, async ({ headers, jwt, database }) => {
     const token = headers["Authorization"]?.replace("Bearer ", "");
     if (!token) {
-      throw new AuthorizationError("Token not found");
+      throw new AuthorizationError(
+        "Token not found",
+        AuthorizationErrorType.INVALID_TOKEN
+      );
     }
 
     const decodedToken = await jwt.verify(token);
     if (!decodedToken) {
-      throw new AuthorizationError("Invalid token");
+      throw new AuthorizationError(
+        "Invalid token",
+        AuthorizationErrorType.INVALID_TOKEN
+      );
     } else {
       const authService = new AuthService();
       authService.userId = decodedToken.userId as string;
