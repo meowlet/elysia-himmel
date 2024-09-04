3be345c747090a2972b5e6ed7b8c3951680bf7b3
meowlet <ngoclink13@gmail.com>
2024-09-04 10:33:16 +0700

    Feature: Refactor email service to use nodemailer for sending emails

--- src/util/Constant.ts
+++ src/util/Constant.ts
@@ -1,11 +1,12 @@
 export class Constant {
   static readonly DATABASE_PATH = "test";
   static readonly PORT = 3000;
+  static readonly FE_URL = "http://localhost:3000";
   static readonly SALT = 8;
-  static readonly ACESS_TOKEN_EXPIRY = "15m";
+  static readonly ACCESS_TOKEN_EXPIRY = "15m";
   static readonly REFRESH_TOKEN_EXPIRY = "7d";
 
-  static readonly ACESS_TOKEN_EXPIRY_MS = 15 * 60 * 1000;
+  static readonly ACCESS_TOKEN_EXPIRY_MS = 15 * 60 * 1000;
   static readonly REFRESH_TOKEN_EXPIRY_MS = 7 * 24 * 60 * 60 * 1000;
 
   static readonly USER_COLLECTION = "users";
