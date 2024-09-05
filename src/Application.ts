e9b89ea98d519a79e981f75bd2bfcb46c695574a
meowlet <ngoclink13@gmail.com>
2024-09-05 20:54:42 +0700

    Chore: Add FictionController for handling fiction-related routes

--- src/Application.ts
+++ src/Application.ts
@@ -2,7 +2,9 @@ import Elysia from "elysia";
 import { UserController } from "./controller/UserController";
 import { AuthController } from "./controller/AuthController";
 import { AuthPlugin } from "./plugin/AuthPlugin";
+import { FictionController } from "./controller/FictionController";
 
 export const Application = new Elysia()
-  .group("/user", (app) => app.use(AuthPlugin).get("/", ""))
+  .group("/user", (app) => app.use(UserController))
+  .group("/fiction", (app) => app.use(FictionController))
   .group("/auth", (app) => app.use(AuthController));
