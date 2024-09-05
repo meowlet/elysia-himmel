58f2a026e02bd3f1462905b38cf392659d8b1a29
meowlet <ngoclink13@gmail.com>
2024-09-05 23:01:50 +0700

    Refactor: Make application routes to use "/me" instead of "/user"

--- src/Application.ts
+++ src/Application.ts
@@ -1,10 +1,9 @@
 import Elysia from "elysia";
-import { UserController } from "./controller/UserController";
-import { AuthController } from "./controller/AuthController";
-import { AuthPlugin } from "./plugin/AuthPlugin";
+import { MeController } from "./controller/MeController";
 import { FictionController } from "./controller/FictionController";
+import { AuthController } from "./controller/AuthController";
 
 export const Application = new Elysia()
-  .group("/user", (app) => app.use(UserController))
+  .group("/me", (app) => app.use(MeController))
   .group("/fiction", (app) => app.use(FictionController))
   .group("/auth", (app) => app.use(AuthController));
