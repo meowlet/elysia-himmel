db90664ba8e65378dbeee18ee1d0f356ed94a3be
meowlet <ngoclink13@gmail.com>
2024-09-22 11:43:09 +0700

    Update

--- src/Index.ts
+++ src/Index.ts
@@ -7,18 +7,7 @@ import { ErrorPlugin } from "./plugin/ErrorPlugin";
 import { AuthorizationError, ConflictError } from "./util/Error";
 import cors from "@elysiajs/cors";
 
-const app = new Elysia({
-  serve: {
-    hostname: "0.0.0.0",
-    // Nếu bạn muốn sử dụng HTTPS:
-    // tls: {
-    //   cert: Bun.file('path/to/cert.pem'),
-    //   key: Bun.file('path/to/key.pem')
-    // },
-    maxRequestBodySize: 1024 * 1024 * 10, // Giới hạn kích thước body request là 10MB
-  },
-  // Các cấu hình khác nếu cần
-})
+const app = new Elysia()
   .use(cors())
   .use(ErrorPlugin)
   .use(Database)
