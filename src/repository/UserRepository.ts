da42b6750d55ba93d83f5adccb22c42211e7de30
meowlet <ngoclink13@gmail.com>
2024-10-16 18:08:37 +0700

    feat(user): implement avatar retrieval endpoint

--- src/repository/UserRepository.ts
+++ src/repository/UserRepository.ts
@@ -1,4 +1,4 @@
-import { Db } from "mongodb";
+import { Db, ObjectId } from "mongodb";
 import { User } from "../model/Entity";
 import { Constant } from "../util/Constant";
 import { database } from "../database/Database";
@@ -9,11 +9,19 @@ export class UserRepository {
   constructor() {
     this.database = database;
   }
+  setProfilePicture(userId: string, picture: File) {
+    throw new Error("Method not implemented.");
+  }
 
   public async getAllUsers(): Promise<Partial<User>[]> {
     return await this.database
       .collection<User>(Constant.USER_COLLECTION)
       .find({}, { projection: { passwordHash: 0 } })
       .toArray();
+  }
+  public async getUserById(userId: string) {
+    return await this.database
+      .collection<User>(Constant.USER_COLLECTION)
+      .findOne({ _id: new ObjectId(userId) });
   }
 }
