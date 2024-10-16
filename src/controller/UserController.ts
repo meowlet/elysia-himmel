82c9914c65ed809bb5def21f4b0f50a21272ff39
meowlet <ngoclink13@gmail.com>
2024-10-16 18:08:25 +0700

    feat(user): add endpoint to get user by ID

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,10 +1,26 @@
-import Elysia from "elysia";
+import Elysia, { NotFoundError } from "elysia";
 import { createSuccessResponse } from "../model/Response";
 import { UserRepository } from "../repository/UserRepository";
+import { join } from "path";
 
-export const UserController = new Elysia().get("/", async ({ set }) => {
-  const repository = new UserRepository();
-  const users = await repository.getAllUsers();
+export const UserController = new Elysia()
+  .get("/", async ({ set }) => {
+    const repository = new UserRepository();
+    const users = await repository.getAllUsers();
 
-  return createSuccessResponse("Lấy danh sách người dùng thành công", users);
-});
+    return createSuccessResponse("Get all users successfully", users);
+  })
+  .get("/:userId", async ({ params }) => {
+    const repository = new UserRepository();
+    const user = await repository.getUserById(params.userId);
+    return createSuccessResponse("Get user successfully", user);
+  })
+  .get("/:userId/avatar", async ({ params }) => {
+    const path = join("public", "users", params.userId, "avatar.jpeg");
+    console.log(path);
+    const file = Bun.file(path);
+    if (!(await file.exists())) {
+      throw new NotFoundError("Avatar not found");
+    }
+    return file;
+  });
