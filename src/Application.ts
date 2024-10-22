a0e72146dfab9ed69244ac622637fde10753474b
meowlet <ngoclink13@gmail.com>
2024-10-22 08:23:16 +0700

    refactor: rename PaymentController to TransactionController
    - Update import statement and route group in Application.ts
    - Remove PaymentController.ts file

--- src/Application.ts
+++ src/Application.ts
@@ -2,7 +2,7 @@ import Elysia from "elysia";
 import { MeController } from "./controller/MeController";
 import { FictionController } from "./controller/FictionController";
 import { AuthController } from "./controller/AuthController";
-import { PaymentController } from "./controller/PaymentController";
+import { TransactionController } from "./controller/TransactionController";
 import { TagController } from "./controller/TagController";
 import { UserController } from "./controller/UserController";
 import { ChapterController } from "./controller/ChapterController";
@@ -12,7 +12,7 @@ export const Application = new Elysia()
   .group("/me", (app) => app.use(MeController))
   .group("/fiction", (app) => app.use(FictionController))
   .group("/auth", (app) => app.use(AuthController))
-  .group("/payment", (app) => app.use(PaymentController))
+  .group("/transaction", (app) => app.use(TransactionController))
   .group("/tag", (app) => app.use(TagController))
   .group("/user", (app) => app.use(UserController))
   .group("/interaction", (app) => app.use(InteractionController));
